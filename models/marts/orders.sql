3b0ae7993be8d81dd6f0b1d093199953ac0679ef
luisleon90 <luis.leon@dbtlabs.com>
2025-07-15 18:59:25 +0000

    pushing new column

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -51,9 +51,7 @@ compute_booleans as (
         order_items_summary.count_order_items,
         order_items_summary.count_food_items > 0 as is_food_order,
         order_items_summary.count_drink_items > 0 as is_drink_order,
-        0 as new_col,
-        14 as life_time_vaule
-
+        0 as new_col
     from orders
 
     left join
